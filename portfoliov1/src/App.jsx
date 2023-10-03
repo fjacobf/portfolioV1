dbe734cc119134a81c6b1969411701ef94884510
fojacob <felipe.jacob.ferreira@gmail.com>
2023-10-03 11:38:57 +0100

    implemented contact page(mostly)

--- portfoliov1/src/App.jsx
+++ portfoliov1/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
     <nav>
         <ul>
           <li><Link to='#home' smooth>Home</Link></li>
-          <li><Link to='#teste' smooth>About Me</Link></li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li><Link to='#about me' smooth>About Me</Link></li>
+          <li><Link to='#projects' smooth>Projects</Link></li>
+          <li><Link to='#contact' smooth>Contact</Link></li>
         </ul>
       </nav>
     </header>
@@ -20,12 +20,31 @@ function App() {
       <h1>I am Felipe Jacob</h1>
       <h2>Developer</h2>
     </div>
-    <section id="teste">
+    <section id="about me">
     <div className='texto'>
       <h1>About Me</h1>
       <p>Hi! I am Felipe, a computer engineering student at University of Porto. I´ve been studying programming since high school and love creating new things!</p>
     </div>
     <img src={img1} alt="personal foto" width="300px" height="300px"/>
+    </section>
+    <section id="projects">
+      <h1>Projects</h1>
+      <div className="projects-trail">
+        <div className="project-card">
+          <h2>App</h2>
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <h1>Contact Me</h1>
+      <form action="">
+        <label htmlFor="FullName">Full Name</label>
+        <input type="text" />
+        <label htmlFor="Email">Email</label>
+        <input type="text" />
+        <label htmlFor="Message">Message</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </form>
     </section>
     </BrowserRouter>
   )
